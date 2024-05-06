b2dc65d37586e5eb29f50e1974b3dd12d79ce003
Jack <jackfperryjr@gmail.com>
2024-05-06 12:00:26 -0500

    Corrected a few things.

--- keggy.py
+++ keggy.py
@@ -60,40 +60,38 @@ def get_monsters(monster_name=None,monster_cr=None):
 
 def get_stat_mod(stat_value):
     match stat_value:
-        case stat_value if stat_value == -1:
+        case stat_value if stat_value == 1:
             return f'{stat_value} (-5)'
-        case stat_value if stat_value in range(-2, -3):
+        case stat_value if stat_value in range(2, 4):
             return f'{stat_value} (-4)'
-        case stat_value if stat_value in range(-4, -5):
+        case stat_value if stat_value in range(4, 6):
             return f'{stat_value} (-3)'
-        case stat_value if stat_value in range(-6, -7):
+        case stat_value if stat_value in range(6, 8):
             return f'{stat_value} (-2)' 
-        case stat_value if stat_value in range(-8, -9):
+        case stat_value if stat_value in range(8, 10):
             return f'{stat_value} (-1)' 
-        case stat_value if stat_value in range(10, 11):
+        case stat_value if stat_value in range(10, 12):
             return f'{stat_value} (+0)' 
-        case stat_value if stat_value in range(12, 13):
+        case stat_value if stat_value in range(12, 14):
             return f'{stat_value} (+1)' 
-        case stat_value if stat_value in range(14, 15):
+        case stat_value if stat_value in range(14, 16):
             return f'{stat_value} (+2)' 
-        case stat_value if stat_value in range(16, 17):
+        case stat_value if stat_value in range(16, 18):
             return f'{stat_value} (+3)' 
-        case stat_value if stat_value in range(18, 19):
+        case stat_value if stat_value in range(18, 20):
             return f'{stat_value} (+4)' 
-        case stat_value if stat_value in range(20, 21):
+        case stat_value if stat_value in range(20, 22):
             return f'{stat_value} (+5)' 
-        case stat_value if stat_value in range(22, 23):
+        case stat_value if stat_value in range(22, 24):
             return f'{stat_value} (+6)' 
-        case stat_value if stat_value in range(24, 25):
+        case stat_value if stat_value in range(24, 26):
             return f'{stat_value} (+7)' 
-        case stat_value if stat_value in range(26, 27):
+        case stat_value if stat_value in range(26, 28):
             return f'{stat_value} (+8)' 
-        case stat_value if stat_value in range(28, 29):
+        case stat_value if stat_value in range(28, 30):
             return f'{stat_value} (+9)' 
         case stat_value if stat_value == 30:
             return f'{stat_value} (+10)' 
-        case _:
-            # no default
 
 @bot.event
 async def on_ready():
@@ -123,6 +121,7 @@ async def on_message(message):
     if bot.user.mentioned_in(message) and 'tell me about' in message.content.lower() and 'monster' in message.content.lower():
         await message.channel.send('Sure, boss! I\'ll send you a private message with details.')
         monster_in_message = re.search(r'tell me about(\s|a|the)(.*?)monster(s?)', message.content).group(2)
+        monster_in_message = monster_in_message.lstrip('a ').lstrip('the ')
         monster_from_api = get_monsters(monster_name=monster_in_message)
 
         if 'error' in monster_from_api:
@@ -269,6 +268,6 @@ async def celebrate(ctx):
     await ctx.send(response)
 
 try:
-    bot.run(os.getenv('DISCORD_TOKEN_DEV'))
+    bot.run(os.getenv('DISCORD_TOKEN'))
 except Exception as e:
     print(f"An error occurred: {e}")
